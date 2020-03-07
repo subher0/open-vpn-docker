0d27f7caf6bb46f309a040914b803e0424abca47
farid <subher0@mail.ru>
2020-03-08 00:03:00 +0300

    Some tweaks

--- scripts/docker/docker_start_ovpn.sh
+++ scripts/docker/docker_start_ovpn.sh
@@ -5,6 +5,7 @@ cp /docker/openvpn-client.conf $SERVER_HOME/client-configs/base.conf
 sed -i -e "s/SERVER_IP_PLACEHOLDER/$SERVER_IP/g" $SERVER_HOME/client-configs/base.conf
 
 cd $SERVER_HOME/client-configs
+/docker/scripts/docker_generate_cert.sh initial
 
 echo "######## CREATING NET DIRS ########"
 mkdir -p /dev/net
