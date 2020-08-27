e2ab70076c5f0a7758cde13043ac4a0e5072528c
Farman Kaziakhmedov <subher0@mail.ru>
2020-08-27 22:02:53 +0300

    Some tweaks to docker installation on ubuntu

--- host_install_docker__ubuntu.sh
+++ host_install_docker__ubuntu.sh
@@ -18,19 +18,30 @@ do
   sleep 1
 done
 
-apt-get remove docker docker-engine docker.io containerd runc
+echo "REMOVING PREVIOUS DOCKER..."
+apt-get remove docker docker.io containerd runc
+
+echo "UPDATING APT AND INSTALLING CURL..."
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common curl
+apt-get install curl
+
+echo "INSTALLING DOCKER REPO..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "UPDATING APT AGAIN..."
 apt-get update
+
+echo "INSTALLING DOCKER..."
 apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "RUNNING HELLO WORLD..."
 docker run hello-world
 
 echo "############## ADDING USER $USER_TO_ADD TO DOCKER USERS ###################"
 usermod -aG docker $USER_TO_ADD
 
 echo "############## INSTALLING DOCKER COMPOSE ####################"
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
